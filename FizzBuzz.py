1298b238eeb93e3902da75c8d2074de58d970cf5
Ivy <njokiivy47@gmail.com>
2022-02-19 19:40:21 +0300

    Created a fizzbuzz game and a password generator

---
+++ FizzBuzz.py
@@ -0,0 +1,26 @@
+student_scores = input("Input a list of student scores\n").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(f"The highest score is {highest_score} ")
+
+total = 0
+for number in range(0, 101, 2):
+    total += number
+print(total)
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
---
+++ main.py
@@ -0,0 +1,22 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + "pie")
+
+student_heights = input("Input a list of student heights?\n").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+print(student_heights)
+
+total_height = 0
+for height in student_heights:
+    total_height += height
+print(total_height)
+
+number_of_students = 0
+for student in student_heights:
+    number_of_students += 1
+print(number_of_students)
+
+average_height = round(total_height / number_of_students)
+print(f"The average height is {average_height}")
